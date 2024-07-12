dc1a2d1111762993f7d1f80f3f9c95853f771dcd
Gabriel Arias <gabriel.arias@edificiosanluis.com>
2024-07-12 16:03:44 -0400

    Added web title and styling

---
+++ frontend/wwwroot/js/site.js
@@ -0,0 +1,18 @@
+
+
+
+
+
+
+
+// const map = L.map('map', {zoomControl : false, worldCopyJump : true}).setView([35.15608617490063, -90.05190639489136], 13);
+
+// L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//     maxZoom: 19,
+//     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+// }).addTo(map);
+
+// var marker = L.marker([35.15608617490063, -90.05190639489136], {draggable: true, autoPan: true}).addTo(map);
+
+
+// marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
