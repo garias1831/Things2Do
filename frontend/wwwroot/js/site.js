f21eae27929a50216db2bd003890b1dc5240c471
Gabriel Arias <gabriel.arias@edificiosanluis.com>
2024-07-16 15:09:19 -0400

    Finished info section

--- frontend/wwwroot/js/site.js
+++ frontend/wwwroot/js/site.js
@@ -1,9 +1,20 @@
 
+function mapSetup() {
+    const initX = 35.15608617490063;
+    const initY = -90.05190639489136;
+    const map = L.map('map', {
+        zoomControl : false, worldCopyJump : true}).setView([initX, initY], 13);
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                maxZoom: 19,
+                attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+             }).addTo(map);
+    var marker = L.marker([initX, initY], {draggable: true, autoPan: true}).addTo(map);
+
+}
 
 
 
-
-
+mapSetup();
 
 // const map = L.map('map', {zoomControl : false, worldCopyJump : true}).setView([35.15608617490063, -90.05190639489136], 13);
 
@@ -15,4 +26,4 @@
 // var marker = L.marker([35.15608617490063, -90.05190639489136], {draggable: true, autoPan: true}).addTo(map);
 
 
-// marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+
