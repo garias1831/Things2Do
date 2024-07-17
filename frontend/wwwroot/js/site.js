e7ecc61779c7df6b67b4c1887c1953a1e3cb0b36
Gabriel Arias <gabriel.arias@edificiosanluis.com>
2024-07-17 14:48:09 -0400

    Added fixed bounds to leaflet map

--- frontend/wwwroot/js/site.js
+++ frontend/wwwroot/js/site.js
@@ -1,15 +1,27 @@
-
+/*Configures the inital map state and options*/
 function mapSetup() {
+    //Initial map + home marker position
     const initX = 35.15608617490063;
     const initY = -90.05190639489136;
-    const map = L.map('map', 
-        {maxZoom : 17, zoomControl : false, worldCopyJump : true}).setView([initX, initY], 13);
+
+    //So user can't wrap around world + see gray zones at poles
+    const bounds = [
+        [-88, -180],
+        [88, 180],
+    ];
+    const map = L.map('map', {
+        maxZoom : 17, 
+        maxBounds: bounds,
+        maxBoundsViscosity: 1.0,
+        zoomControl : false}).setView([initX, initY], 13);
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
                 minZoom: 3,
                 attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
              }).addTo(map);
-    var marker = L.marker([initX, initY], {draggable: true, autoPan: true}).addTo(map);
 
+    //NOTE -- will have to put this somewhere accessible (to get things like location )
+    const homemarker = L.marker([initX, initY], {draggable: true, autoPan: true}).addTo(map);
+    
 }
 
 
